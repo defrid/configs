6d456f691ea1f0162318737067a787813dabbcf8
Pavel Beketov <pavel.beketov@kandasoft.com>
2015-07-09 09:29:36 +0300

    Last changes

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -285,3 +285,5 @@
 (global-set-key [f3] 'highlight-symbol-next)
 (global-set-key [(shift f3)] 'highlight-symbol-prev)
 (global-set-key [(meta f3)] 'highlight-symbol-query-replace)
+
+(add-to-list 'auto-mode-alist '("\\.scss$" . web-mode))
