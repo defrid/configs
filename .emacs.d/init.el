1f50a80ff101f0edf0a43f35593e74a83de731a2
defrid <defrid73@gmail.com>
2015-07-10 15:31:27 +0300

    HZ

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -214,3 +214,39 @@
 (require 'redo+)
 (require 'redo+)
 (global-set-key (kbd "C-?") 'redo)
+
+
+(require 'web-beautify) ;; Using web-beautify
+
+(eval-after-load 'js2-mode
+	'(define-key js2-mode-map (kbd "C-c b") 'web-beautify-js))
+(eval-after-load 'json-mode
+	'(define-key json-mode-map (kbd "C-c b") 'web-beautify-js))
+(eval-after-load 'sgml-mode
+	'(define-key html-mode-map (kbd "C-c b") 'web-beautify-html))
+(eval-after-load 'css-mode
+	'(define-key css-mode-map (kbd "C-c b") 'web-beautify-css))
+
+;;auto-beautify on saving
+(add-hook 'js-mode-hook
+		  (lambda () (add-hook 'before-save-hook 'web-beautify-js-buffer t t)))
+
+(eval-after-load 'js2-mode
+	'(add-hook 'js2-mode-hook
+	  (lambda ()
+		  (add-hook 'before-save-hook 'web-beautify-js-buffer t t))))
+
+(eval-after-load 'json-mode
+	'(add-hook 'json-mode-hook
+	  (lambda ()
+		  (add-hook 'before-save-hook 'web-beautify-js-buffer t t))))
+
+(eval-after-load 'sgml-mode
+	'(add-hook 'html-mode-hook
+	  (lambda ()
+		  (add-hook 'before-save-hook 'web-beautify-html-buffer t t))))
+
+(eval-after-load 'css-mode
+	'(add-hook 'css-mode-hook
+	  (lambda ()
+		  (add-hook 'before-save-hook 'web-beautify-css-buffer t t))))
